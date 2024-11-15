da4843cdb811f21f779c23652b08a403cd051cfd
UrielShapiro <uriel1999@gmail.com>
2024-11-16 00:18:56 +0200

    Added print statements

--- FlowAnalyzer.py
+++ FlowAnalyzer.py
@@ -22,7 +22,18 @@ class FlowAnalyzer:
         self.__flow_analysis[PacketAnalyzer.get_five_tuple(packet)] = PacketAnalyzer(packet)
 
     def print(self):
+        total_number_of_packets = 0
+        total_number_of_bytes = 0
         for flow in self.__flow_analysis.values():
             flow.print()
+            print("Amount of bytes transferred on this flow: " + str(flow.total_data) + " bytes")
+            print("Amount of packets transferred on this flow: " + str(len(flow.packets)) + " packets")
+            if flow.abnormalities.__len__() > 0:
+                print("Abnormalities:" + str(flow.abnormalities))
+            total_number_of_packets += len(flow.packets)
+            total_number_of_bytes += flow.total_data
+
         print("Flow analysis complete")
+        print("Total number of bytes: ", total_number_of_bytes)
+        print("Total number of packets: ", total_number_of_packets)
         print("Number of flows: ", len(self.__flow_analysis))
