aa925a2d9771cabf2b940fdcde9db5b8b845a956
UrielShapiro <uriel1999@gmail.com>
2024-11-15 22:12:11 +0200

    Added __contains__ function to check if a packet is in the flow already. Also added 2 methods to add a packet to the flow and update an existing PacketAnalyzer

--- FlowAnalyzer.py
+++ FlowAnalyzer.py
@@ -12,7 +12,24 @@ from PacketExceptions import TCPFlagsException, UDPPortException, MacAddressExce
 
 class FlowAnalyzer:
     def __init__(self):
-        self.__flow_analysis = Dict[PacketAnalyzer, List[str, list()]]
+        self.__flow_analysis = {}
+
+    def __contains__(self, packet: scapy.Packet) -> bool:
+        return PacketAnalyzer.get_five_tuple(packet) in self.__flow_analysis.keys()
+
 
     def update_packet(self, packet: scapy.Packet):
-        PacketAnalyzer.analyze_packet(packet)
+        print("Updating packet")
+        if PacketAnalyzer.get_five_tuple(packet) not in self.__flow_analysis.keys():
+            raise Exception("Packet not found in flow analysis")
+        self.__flow_analysis.get(PacketAnalyzer.get_five_tuple(packet)).update_packet(packet)
+
+    def add_packet(self, packet: scapy.Packet):
+        print("Adding packet")
+        self.__flow_analysis[PacketAnalyzer.get_five_tuple(packet)] = PacketAnalyzer(packet)
+
+    def print(self):
+        for flow in self.__flow_analysis.values():
+            flow.print()
+        print("Flow analysis complete")
+        print("Number of flows: ", len(self.__flow_analysis))
