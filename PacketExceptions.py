50bc38ee32fbc397a54b94ef18ab5ce9598909b8
UrielShapiro <uriel1999@gmail.com>
2024-11-20 02:01:31 +0200

    Changed the naming for the UDP Exception to be more general

--- PacketExceptions.py
+++ PacketExceptions.py
@@ -6,13 +6,15 @@ class TCPFlagsException(Exception):
     def __str__(self):
         return f"TCPFlagsException: {self.message}"
 
-class UDPPortException(Exception):
+
+class UDPException(Exception):
     def __init__(self, message):
         super().__init__(message)
         self.message = message
 
     def __str__(self):
-        return f"UDPPortException: {self.message}"
+        return f"UDPException: {self.message}"
+
 
 class MacAddressException(Exception):
     def __init__(self, message):
