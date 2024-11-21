8e6b1f4f905af9830eedd2776818b38ab2fd5e76
UrielShapiro <uriel1999@gmail.com>
2024-11-21 04:09:49 +0200

    Added new logger, optimized update_packet method. The print method will only print flows that had abnormalities

--- FlowAnalyzer.py
+++ FlowAnalyzer.py
@@ -1,33 +1,13 @@
-import logging
-import os
-from logging.handlers import RotatingFileHandler
-
 import humanfriendly
 import scapy.all as scapy
 
+import logging_setup
 from PacketAnalyzer import PacketAnalyzer
 
 
 class FlowAnalyzer:
-    # Set up a rotating file handler
-    file_handler = None
-    try:
-        file_handler = RotatingFileHandler(
-            "./logs/FlowAnalyzer.log", maxBytes=5 * 1024 * 1024)  # 5 MB log file
-
-    except FileNotFoundError:
-        os.mkdir("logs")
-
-    logging.basicConfig(
-        level=logging.DEBUG,  # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",  # Format for log messages
-        handlers=[
-            file_handler  # Logs to a file
-        ]
-    )
-
     def __init__(self):
-        self.logger = logging.getLogger(__name__)
+        self.logger = logging_setup.get_logger(self.__class__.__name__)
         self.__flow_analysis = {}
 
     def __contains__(self, packet: scapy.Packet) -> bool:
@@ -36,16 +16,18 @@ class FlowAnalyzer:
             (dst_ip, src_ip, dst_port, src_port, protocol) in self.__flow_analysis.keys()
 
     def update_packet(self, packet: scapy.Packet):
-        self.logger.info("Updating flow with packet: " + packet.summary())
+        self.logger.debug(f"Updating flow with packet: {packet.summary()}")
         src_ip, dst_ip, src_port, dst_port, protocol = PacketAnalyzer.get_five_tuple(packet)
-        if (src_ip, dst_ip, src_port, dst_port, protocol) not in self.__flow_analysis.keys() and \
-                (dst_ip, src_ip, dst_port, src_port, protocol) not in self.__flow_analysis.keys():
-            self.logger.error(
-                "Tried to add a packet to an existing flow but did not find a matching flow: " + packet.summary())
-        if (src_ip, dst_ip, src_port, dst_port, protocol) in self.__flow_analysis.keys():
-            self.__flow_analysis.get((src_ip, dst_ip, src_port, dst_port, protocol)).update_packet(packet)
+        flow_key = (src_ip, dst_ip, src_port, dst_port, protocol)
+        reverse_flow_key = (dst_ip, src_ip, dst_port, src_port, protocol)
+
+        if flow_key in self.__flow_analysis:
+            self.__flow_analysis[flow_key].update_packet(packet)
+        elif reverse_flow_key in self.__flow_analysis:
+            self.__flow_analysis[reverse_flow_key].update_packet(packet)
         else:
-            self.__flow_analysis.get((dst_ip, src_ip, dst_port, src_port, protocol)).update_packet(packet)
+            self.logger.error("Attempted to update a non-existent flow.")
+            return
 
     def add_packet(self, packet: scapy.Packet):
         self.logger.info("Adding packet: " + packet.summary())
@@ -62,14 +44,16 @@ class FlowAnalyzer:
         total_number_of_bytes = 0
         index = 1
         for flow in self.__flow_analysis.values():
-            print(f"Flow: {index}:")
+            abnormalities = flow.get_abnormalities()
+            if len(list(abnormalities)) > 0:
+                print(f"Flow: {index}:")
+                flow.print()
+                print("Amount of bytes transferred on this flow: " + str(flow.total_data) + " bytes")
+                print("Amount of packets transferred on this flow: " + str(flow.num_of_packets) + " packets")
+                print()  # Add a newline
             index += 1
-            flow.print()
-            print("Amount of bytes transferred on this flow: " + str(flow.total_data) + " bytes")
-            print("Amount of packets transferred on this flow: " + str(len(flow.packets)) + " packets")
-            total_number_of_packets += len(flow.packets)
+            total_number_of_packets += flow.num_of_packets
             total_number_of_bytes += flow.total_data
-            print()  # Add a newline
         print("Flow analysis complete")
         print("Total size of packets: ", humanfriendly.format_size(total_number_of_bytes))
         print("Total number of packets: ", total_number_of_packets)
