222d66c16d9885d44191537375d266aff71d5a0a
UrielShapiro <uriel1999@gmail.com>
2024-12-05 01:53:16 +0200

    Removed unused exceptions

--- PacketExceptions.py
+++ PacketExceptions.py
@@ -5,21 +5,3 @@ class TCPFlagsException(Exception):
 
     def __str__(self):
         return f"TCPFlagsException: {self.message}"
-
-
-class UDPException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
-
-    def __str__(self):
-        return f"UDPException: {self.message}"
-
-
-class MacAddressException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
-
-    def __str__(self):
-        return f"MacAddressException: {self.message}"
